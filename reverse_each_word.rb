333bc5d0421ab9a2890fe698a24b4faf6135c8aa
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-18 17:43:43 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-sentence1 = "Hello there, and how are you?"
-sentence2 = "Hi again, just making sure it's reversed!"
-
-def reverse_each_word(sentence1, sentence2)
-  reverse_each_word collect. do
-  end
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end
