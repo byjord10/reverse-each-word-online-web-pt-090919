c49a62a8454b14d7747c280e59201df3ba9a1182
Brianna Jordan <bryvjordan9@gmail.com>
2019-09-18 17:42:55 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+sentence1 = "Hello there, and how are you?"
+sentence2 = "Hi again, just making sure it's reversed!"
+
+def reverse_each_word(sentence1, sentence2)
+  reverse_each_word collect. do
+  end
+end
